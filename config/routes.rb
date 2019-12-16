0b2cdc8f2fe352bc3dc280583d63d221a5fd9bc3
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-16 23:23:17 +0700

    add teams, stocks, users and admin devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :teams, path: 'teams'
+  devise_for :stocks, path: 'stocks'
+  devise_for :users, path: 'users'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :admins
+  devise_for :admins, path: 'admins'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
