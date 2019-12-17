8ef7ed496bd26282a55a84f50b648df06e681828
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-18 00:30:51 +0700

    integrate stock with wallet concern

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
+  include Wallet
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end
