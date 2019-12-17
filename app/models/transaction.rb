ef0c0d93da02f8a09223da178520342b80a0d6b1
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-18 00:31:31 +0700

    add validation and make targetable as optional

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,16 @@
 class Transaction < ApplicationRecord
   belongs_to :sourceable, polymorphic: true
-  belongs_to :targetable, polymorphic: true
+  belongs_to :targetable, polymorphic: true, optional: true 
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validate :check_current_balance, :check_self_transfer, on: :create
+
+  private
+  def check_current_balance
+    errors.add(:amount, 'is not enough to process this transcation') if sourceable.balance - amount < 0
+  end
+
+  def check_self_transfer
+    errors.add(:targetable, 'cannot same as transfer source') if sourceable.eql?(targetable)
+  end
 end
