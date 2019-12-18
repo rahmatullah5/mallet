7a5602822be0ec2a1938298cab467eab260334cc
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-18 18:29:31 +0700

    add deposits and withdraws

--- app/models/concerns/wallet.rb
+++ app/models/concerns/wallet.rb
@@ -3,25 +3,32 @@ module Wallet
 
   included do
     after_create :init_default_balance
-    has_many :withdraws, -> { where(targetable: nil) }, class_name: 'Transaction', as: :sourceable
     has_many :credits, -> { where.not(targetable: nil) }, class_name: 'Transaction', as: :sourceable
-    has_many :debits, class_name: 'Transaction', as: :targetable
+    has_many :withdraws, -> { where(targetable: nil) }, class_name: 'Transaction', as: :sourceable
+    has_many :debits, -> { where.not(sourceable: nil) }, class_name: 'Transaction', as: :targetable
+    has_many :deposits, -> { where(sourceable: nil) }, class_name: 'Transaction', as: :targetable
   end
 
   def transfer(target, amount)
     ActiveRecord::Base.transaction do
-      credits.create!(targetable: target, amount: amount)  
+      credits.create(targetable: target, amount: amount)  
     end
   end
 
   def withdraw(amount)
     ActiveRecord::Base.transaction do
-      credits.create!(amount: amount)
+      credits.create(amount: amount)
+    end
+  end
+
+  def deposit(amount)
+    ActiveRecord::Base.transaction do
+      deposits.create(amount: amount)
     end
   end
 
   def balance
-    sum_of_debits - (sum_of_credits + sum_of_witdraw)
+    (sum_of_debits + sum_of_deposits) - (sum_of_credits + sum_of_withdraws)
   end
 
   def sum_of_debits
@@ -32,8 +39,16 @@ module Wallet
     credits.sum(:amount)
   end
 
-  def sum_of_witdraw
+  def sum_of_withdraws
     withdraws.sum(:amount)
+  end
+
+  def sum_of_deposits
+    deposits.sum(:amount)
+  end
+
+  def transactions_count
+    debits.count + withdraws.count + credits.count
   end
 
   private
