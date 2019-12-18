27b0acf1bd0e4e5f42083b72d50d47dbd25f59bb
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-18 18:28:27 +0700

    handle user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user, :authenticate_logged_user!
+  
+  def set_user
+    @current_logged_user = current_user || current_stock || current_team || current_admin
+  end
+
+  private 
+
+  def check_role
+    raise ActionController::RoutingError.new('Not Found') if @current_logged_user.class.to_s.eql?("Admin")
+  end
+
+  def authenticate_logged_user!
+    class_name = @current_logged_user  ? @current_logged_user.class.to_s.downcase : 'user'
+    eval("authenticate_#{class_name}!")
+  end
 end
