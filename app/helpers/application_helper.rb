89b85578082eb7829ac55dc30d7c690154c45a95
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-18 18:28:57 +0700

    add handler user and currency helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def number_to_currency_id(number)
+    number_to_currency(number, :unit => "Rp ", :separator => ",", :delimiter => ".")
+  end
+
+  def any_user_signed_in?
+    user_signed_in? || stock_signed_in? || team_signed_in?
+  end
 end
